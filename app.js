faedafcc135e0d02c41988635a87a9dc879ea0fe
Akhilesh kumar <151368664+AKHILESHKUMAR11@users.noreply.github.com>
2026-05-31 13:51:03 +0530

    Update response message to VERSION 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Working VERSION 4");
+  res.send("CI/CD Pipeline Working VERSION 5");
 });
 
 app.listen(3000);
