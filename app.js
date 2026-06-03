8e4184938c6e09bbf6fbbbad049f4bb5c837155b
Akhilesh kumar <151368664+AKHILESHKUMAR11@users.noreply.github.com>
2026-06-03 17:58:58 +0530

    Update response message to VERSION 7

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Working VERSION 6");
+  res.send("CI/CD Pipeline Working VERSION 7");
 });
 
 app.listen(3000);
